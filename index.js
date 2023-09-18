df53f501b66ccfe4d6c40b3f8f70cf54a8db5c9a
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-09-18 13:11:44 -0400

    class extension lab done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+  get countSides() {
+    return this.sides.length;
+  }
+  get perimeter() {
+    return this.sides.reduce((acc, side) => acc + side);
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    if (
+      this.sides[0] + this.sides[1] > this.sides[2] &&
+      this.sides[0] + this.sides[2] > this.sides[1] &&
+      this.sides[1] + this.sides[2] > this.sides[0]
+    ) {
+      return true;
+    }
+    return false;
+  }
+}
+class Square extends Polygon {
+  get isValid() {
+    for (let i = 0; i < 3; i++) {
+      if (this.sides[i] !== this.sides[i + 1]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  get area() {
+    return Math.pow(this.sides[0], 2);
+  }
+}
